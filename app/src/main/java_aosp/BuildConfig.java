d05c75f4e758c8393d034bf74f230ac97b7039fa
Erfan Abdi <erfangplus@gmail.com>
2022-07-02 21:52:42 +0430

    Auxio: Add soong blueprints and libs
    * Also rebranded application id for LMODroid

---
+++ app/src/main/java_aosp/BuildConfig.java
@@ -0,0 +1,7 @@
+package org.oxycblt.auxio;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.libremobileos.music";
+
+    public static final boolean DEBUG = false;
+}
